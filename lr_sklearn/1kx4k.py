1f82aa033266af38ce5d2da9e15aef79ed85fec4
Leiming Yu <leimingyu830@gmail.com>
2016-12-28 22:54:20 -0500

    logistic regression, sklearn

---
+++ lr_sklearn/1kx4k.py
@@ -0,0 +1,84 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+import time
+
+npoints = 1000
+nfeatures = 4096 
+
+#------------------------------------------------------------------------------
+# 1 cpu thread
+#------------------------------------------------------------------------------
+
+
+print  '1K x 4K using 1 thread'
+
+X = np.random.rand(npoints, nfeatures)
+y = np.random.randint(2, size=npoints)
+
+logreg = LogisticRegression(n_jobs=1)
+
+start = time.time()
+logreg.fit(X, y)
+end = time.time()
+
+
+n_iter = logreg.n_iter_
+runtime = end - start
+iter_time = runtime / n_iter
+
+print str(runtime) +  ' s'
+print 'runtime per iter : ' + str(iter_time) + ' s'
+print 'niter : ' + str(n_iter)
+print '\n\n'
+
+
+#------------------------------------------------------------------------------
+# 4 cpu thread
+#------------------------------------------------------------------------------
+
+print  '1K x 4K using 4 thread'
+
+X = np.random.rand(npoints, nfeatures)
+y = np.random.randint(2, size=npoints)
+
+logreg = LogisticRegression(n_jobs=4)
+
+start = time.time()
+logreg.fit(X, y)
+end = time.time()
+
+
+n_iter = logreg.n_iter_
+runtime = end - start
+iter_time = runtime / n_iter
+
+print str(runtime) +  ' s'
+print 'runtime per iter : ' + str(iter_time) + ' s'
+print 'niter : ' + str(n_iter)
+print '\n\n'
+
+#------------------------------------------------------------------------------
+# 8 cpu thread
+#------------------------------------------------------------------------------
+
+print  '1K x 4K using 8 thread'
+
+X = np.random.rand(npoints, nfeatures)
+y = np.random.randint(2, size=npoints)
+
+logreg = LogisticRegression(n_jobs=8)
+
+start = time.time()
+logreg.fit(X, y)
+end = time.time()
+
+
+n_iter = logreg.n_iter_
+runtime = end - start
+iter_time = runtime / n_iter
+
+print str(runtime) +  ' s'
+print 'runtime per iter : ' + str(iter_time) + ' s'
+print 'niter : ' + str(n_iter)
+print '\n\n'
